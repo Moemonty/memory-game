6635f62c49ca0a7e1faad80a90c33ad857cea7f2
Moises Montenegro <moemonty@gmail.com>
2018-12-04 21:09:28 -0600

    Added new folder

---
+++ src/containers/CardGridContainer.js
@@ -0,0 +1,25 @@
+// CardGridContainer.js
+import React, { Component } from 'react';
+
+const cardStyle = {
+  border: 'solid 2px #fff',
+  borderRadius: '3px',
+  background: '#000', 
+  color: '#fff',
+  minHeight: 200,
+  padding: '10px'
+};
+
+class CardGridContainer extends Component {
+  render() {
+    return (
+      <div id="card-grid">
+        <div className="card" style={cardStyle}>
+          Ace of Spades
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CardGridContainer;
