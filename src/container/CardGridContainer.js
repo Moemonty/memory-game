d45dec34749b39ea0d070ca61c2e594a92a1c803
Moemonty <moemonty@gmail.com>
2018-12-07 09:20:48 -0600

    Updated flips

--- src/container/CardGridContainer.js
+++ src/container/CardGridContainer.js
@@ -15,50 +15,55 @@ class CardGridContainer extends Component {
 
   // Public class fields syntax
   flipCard = event => {
-    console.log(event.target.innerText);
+    console.log(event.currentTarget);
     const name = event.target.innerText;
+    event.currentTarget.classList.add('flip');
 
     if (this.state.firstCard === null) {
       this.setState({
         firstCard: name
       });
+
     } else {
       console.log('card already set');
     }
 
     console.log(this.state, ' is state');
-    console.log(this, 'card clicked');
   }
 
   render() {
     return (
       <div className="div">
         <div className="CardGridContainer">
-          {/* onCardFlip prop takes flipCard function as param */}
+          {/*
+            @TODO: Iterate through a date list
+            that will dynamically create each card from a data list
+            onCardFlip prop takes flipCard function as param
+          */}
           <Card name="Ace of Spades" onCardFlip={ this.flipCard } />
-          <Card name="Ace of Hearts" />
-          <Card name="Ace of Clubs" />
-          <Card name="Ace of Diamonds" />
-          <Card name="Ace of Spades" />
-          <Card name="Ace of Hearts" />
-          <Card name="Ace of Clubs" />
-          <Card name="Ace of Diamonds" />
-          <Card name="Jack of Spades" />
-          <Card name="Jack of Hearts" />
-          <Card name="Jack of Clubs" />
-          <Card name="Jack of Diamonds" />
-          <Card name="Jack of Spades" />
-          <Card name="Jack of Hearts" />
-          <Card name="Jack of Clubs" />
-          <Card name="Jack of Diamonds" />
-          <Card name="Queen of Spades" />
-          <Card name="Queen of Hearts" />
-          <Card name="Queen of Clubs" />
-          <Card name="Queen of Diamonds" />
-          <Card name="Queen of Spades" />
-          <Card name="Queen of Hearts" />
-          <Card name="Queen of Clubs" />
-          <Card name="Queen of Diamonds" />
+          <Card name="Ace of Hearts" onCardFlip={ this.flipCard } />
+          <Card name="Ace of Clubs" onCardFlip={ this.flipCard } />
+          <Card name="Ace of Diamonds" onCardFlip={ this.flipCard }/>
+          <Card name="Ace of Spades" onCardFlip={ this.flipCard } />
+          <Card name="Ace of Hearts" onCardFlip={ this.flipCard } />
+          <Card name="Ace of Clubs" onCardFlip={ this.flipCard } />
+          <Card name="Ace of Diamonds" onCardFlip={ this.flipCard } />
+          <Card name="Jack of Spades" onCardFlip={ this.flipCard } />
+          <Card name="Jack of Hearts" onCardFlip={ this.flipCard } />
+          <Card name="Jack of Clubs" onCardFlip={ this.flipCard } />
+          <Card name="Jack of Diamonds" onCardFlip={ this.flipCard } />
+          <Card name="Jack of Spades" onCardFlip={ this.flipCard } />
+          <Card name="Jack of Hearts" onCardFlip={ this.flipCard } />
+          <Card name="Jack of Clubs" onCardFlip={ this.flipCard } />
+          <Card name="Jack of Diamonds" onCardFlip={ this.flipCard } />
+          <Card name="Queen of Spades" onCardFlip={ this.flipCard } />
+          <Card name="Queen of Hearts" onCardFlip={ this.flipCard } />
+          <Card name="Queen of Clubs" onCardFlip={ this.flipCard } />
+          <Card name="Queen of Diamonds" onCardFlip={ this.flipCard } />
+          <Card name="Queen of Spades" onCardFlip={ this.flipCard } />
+          <Card name="Queen of Hearts" onCardFlip={ this.flipCard } />
+          <Card name="Queen of Clubs" onCardFlip={ this.flipCard } />
+          <Card name="Queen of Diamonds" onCardFlip={ this.flipCard } />
         </div>
       </div>
     );
--- src/presentational/Card.jsx
+++ src/presentational/Card.jsx
@@ -7,7 +7,12 @@ const Card = props => {
   const { name, onCardFlip } = props;
   return (
     <div className="card" onClick={ onCardFlip }>
-      { name }
+      <div className="card__front-face">
+        { name }
+      </div>
+      <div className="card__back-face">
+        Game Card
+      </div>
     </div>
   );
 };
