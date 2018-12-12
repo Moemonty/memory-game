c8b42bc9ae59495f6763b11d20a087c2381f7b0c
Moises Montenegro <mmontenegro@shoprunner.com>
2018-12-12 09:29:29 -0600

    Wrote test on application behavior, examine component UI versus state?

--- src/container/CardGridContainer.test.js
+++ src/container/CardGridContainer.test.js
@@ -9,22 +9,66 @@ import CardGridContainer from './CardGridContainer';
 import Card from '../presentational/Card';
 
 describe('CardGridContainer', () => {
-  let wrapper = '';
-  let componentInstance = '';
+  // let wrapper = '';
+  // let componentInstance = '';
 
-  beforeEach(() => {
-    wrapper = shallow((
-      <CardGridContainer />
-    ));
-    componentInstance = wrapper.instance();
-  });
+  // beforeEach(() => {
+  //   wrapper = shallow((
+  //     <CardGridContainer />
+  //   ));
+  //   componentInstance = wrapper.instance();
+  // });
 
   test('renders the CardGridContainer component with dataset of 24 items', () => {
-    expect(componentInstance.state.message).toBe('Game Start')
+    const wrapper = shallow((
+      <CardGridContainer />
+    ));
+    expect(wrapper.find('Card')).toBeDefined();
+    expect(wrapper.find('Card')).toHaveLength(24);
   });
-
 
   test('renders the CardGridContainer message state with game start', () => {
-    expect(componentInstance.state.message).toBe('Game Start')
+    const wrapper = shallow((
+      <CardGridContainer />
+    ));
+    const componentInstance = wrapper.instance();
+
+    expect(componentInstance.state.message).toBe('Game Start');
+    console.log(wrapper.debug(), ' is the wrapper');
   });
+
+
+    test.only('game message to be Matched! if cards are the same', () => {
+      const wrapper = mount((
+        <CardGridContainer />
+      ));
+
+      const componentInstance = wrapper.instance();
+      // http://enthudrives.com/blog/how-to-test-react-state/
+      // console.log(wrapper.debug());
+
+      const cardBoot = wrapper.find('[data-name="Boot"]').forEach((node, index) => {
+        // NO MATCH
+        // if(index === 0 ){
+        //   node.simulate('click');
+        // } else {
+        //   console.log('no click');
+        // }
+
+        node.simulate('click');
+      });
+
+      // cardBoot[0].simulate('click');
+
+      // console.log('test');
+
+      // console.log(cardBoot.debug() , ' is the boot');
+
+      wrapper.update();
+
+      // expect(componentInstance.state.message).toBe('Game Start');
+
+      expect(componentInstance.state.message).toBe('Matched!');
+      // console.log(wrapper.debug(), ' is the wrapper');
+    });
 });
