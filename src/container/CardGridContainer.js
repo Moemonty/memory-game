45fbd5ea700b67dca577e782d6f8d8e42cec1d2c
Moises Montenegro <mmontenegro@shoprunner.com>
2018-12-13 00:46:42 -0600

    Updated with prop types, finished tests

--- src/container/CardGridContainer.js
+++ src/container/CardGridContainer.js
@@ -4,6 +4,8 @@ import Card from '../presentational/Card';
 import './CardGridContainer.css';
 import loteriaCard from '../images/loteriaCard.jpg';
 
+import PropTypes from 'prop-types';
+
 class CardGridContainer extends Component {
   constructor(props) {
     const { cardSet } = props;
@@ -24,27 +26,27 @@ class CardGridContainer extends Component {
   }
 
   // Known Shuffle Algo
-shuffleCardSet (array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+  shuffleCardSet (array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
   }
-  return array;
-}
 
-shuffledCards (cardSet) {
-  // concat the same cardset to double array
-  const pairedCards = cardSet.concat(cardSet);
-  // shuffle cards
-  return this.shuffleCardSet(pairedCards);
-}
+  shuffledCards (cardSet) {
+    // concat the same cardset to double array
+    const pairedCards = cardSet.concat(cardSet);
+    // shuffle cards
+    return this.shuffleCardSet(pairedCards);
+  }
 
   resetBoard = () => {
     this.setState({
@@ -213,6 +215,10 @@ shuffledCards (cardSet) {
       </div>
     );
   }
+}
+
+CardGridContainer.propTypes = {
+  cardSet: PropTypes.array.isRequired
 }
 
 export default CardGridContainer;
--- src/presentational/Card.jsx
+++ src/presentational/Card.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import './Card.css';
 
+import PropTypes from 'prop-types';
+
 const Card = props => {
 
   const { name, img, onCardFlip, cardBack } = props;
@@ -11,10 +13,17 @@ const Card = props => {
         <img className="card__img" src={img} alt={name} />
       </div>
       <div className="card__back-face">
-        <img src={cardBack} className="card__img"/>
+        <img alt={name} src={cardBack} className="card__img"/>
       </div>
     </div>
   );
 };
+
+Card.propTypes = {
+  name: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  onCardFlip: PropTypes.func.isRequired,
+  cardBack: PropTypes.string.isRequired,
+}
 
 export default Card;
